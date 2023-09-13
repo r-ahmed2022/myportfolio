e1584fc929562777dec1b5f64274e35eabcaf9cc
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-09-13 19:35:25 +0530

    updated

--- src/components/aboutme/Aboutme.jsx
+++ src/components/aboutme/Aboutme.jsx
@@ -1,13 +1,20 @@
 import './aboutme.scss'
 import Skill from './Skill'
 export const Aboutme = () => {
-
+  const openResumePDF = () => {
+    const pdfURL = 'Riyaz-CV-developer.pdf';
+    window.open(pdfURL, '_blank');
+  };
   return (
     <div  className='aboutme' id="aboutme">
     <div className="left">
         <h1>Developer based in Kashmir, India!</h1>
-        <span>Minimalistic design caters well to business</span>
-
+        <ul className="statistics">
+            <li><small>48</small><small>projects</small></li>
+            <li><small>48</small><small>projects</small></li>
+        </ul>
+        <button type="button" className="resume"
+         onClick={openResumePDF}>My Resume</button>
     </div>
     <div className="right">
       <h1>Skills</h1>
