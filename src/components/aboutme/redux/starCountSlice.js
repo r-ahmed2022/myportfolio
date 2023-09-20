d908d6744152b3ac576de4a9a0587dc5ef4510e1
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-09-20 20:32:24 +0530

    upated package.json

--- src/components/aboutme/redux/starCountSlice.js
+++ src/components/aboutme/redux/starCountSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const username = 'r-ahmed2022';
-const token = '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';
+const token = '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';
 
 export const starCountInfo = createAsyncThunk('starCountInfo', async () => {
   const response = await fetch(`https://api.github.com/users/${username}/repos`, {
