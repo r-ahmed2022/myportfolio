c77dc8d25da39d615c489a58f57982401b55d3a1
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-09-27 10:09:10 +0530

    updating projects description

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const cssPortfolio = [
         'images/cssmagic.png',
     url: 'https://r-ahmed2022.github.io/CSS-magic/',
     githublink: 'https://github.com/r-ahmed2022/CSS-magic',
-    description: 'showing power of css',
+    description: 'A app showcasing different container layouts in response to different screen sizes',
     techstack: ['html', 'css'],
   },
   {
@@ -16,17 +16,17 @@ export const cssPortfolio = [
         'images/portfolio.png',
     url: 'https://riysaim.netlify.app/',
     githublink: 'https://github.com/r-ahmed2022/Portfoli-o',
-    description: 'My portfolio in pure Html/Css',
+    description: 'My first portfolio using only HTML/CSS',
     techstack: ['html', 'css'],
   },
   {
     id: 3,
-    title: 'Natours',
+    title: 'Tour&Travels',
     img:
         'images/natours.png',
     url: 'https://r-ahmed2022.github.io/Natours/',
     githublink: 'https://github.com/r-ahmed2022/Natours',
-    description: 'show casing css skill',
+    description: 'Tourn & Travels site, showcasing advanced css, and animation',
     techstack: ['html', 'css'],
   },
 ];
@@ -38,7 +38,7 @@ export const javascriptPortfolio = [
     img: 'images/personal-dashboard.png',
     url: 'https://r-ahmed2022.github.io/Personal-Dashboard/',
     githublink: 'https://github.com/r-ahmed2022/Personal-Dashboard',
-    description: 'A useful chrome extension for personal use',
+    description: 'Chrome extension having todolist tool, 24-hr crypto info, local weather , current time, and inspiring quotes',
     techstack: ['html', 'css', 'javscript'],
   },
   {
@@ -47,7 +47,7 @@ export const javascriptPortfolio = [
     img: 'images/deckofcards.png',
     url: 'https://r-ahmed2022.github.io/DeckofCards-Game/',
     githublink: 'https://github.com/r-ahmed2022/DeckofCards-Game',
-    description: 'A card game coded in javascript',
+    description: 'A card game showcasiung advanced javascript skills',
     techstack: ['html', 'css', 'javscript'],
   },
   {
@@ -56,7 +56,7 @@ export const javascriptPortfolio = [
     img: 'images/movieapp.png',
     url: 'https://r-ahmed2022.github.io/moviesApp/',
     githublink: 'https://github.com/r-ahmed2022/moviesApp',
-    description: 'A movie app',
+    description: 'A movie app enabling movies search, adding favorite movies to watchlist',
     techstack: ['html', 'css', 'javscript'],
 
   },
@@ -66,7 +66,7 @@ export const javascriptPortfolio = [
     img: 'images/weather.png',
     url: 'https://r-ahmed2022.github.io/Weather-App/',
     githublink: 'https://github.com/r-ahmed2022/Weather-App',
-    description: 'displaying weather of different cities',
+    description: 'An app providing weather info about top 10 cities, local weather, besides enabling user likes/comments ',
     techstack: ['html', 'css', 'javscript'],
 
   },
@@ -80,7 +80,7 @@ export const reactPortfolio = [
         'images/ramozon.png',
     url: 'https://r-ahmed2022.github.io/ramozon-commerce/',
     githublink: 'https://github.com/r-ahmed2022/ramozon-commerce',
-    description: 'An simple E-commerce react app',
+    description: 'An simple E-commerce SPA developed in React',
     techstack: ['html/css', 'javscript', 'React'],
   },
   {
@@ -90,7 +90,7 @@ export const reactPortfolio = [
         'images/cryptohub.png',
     url: 'https://r-ahmed2022.github.io/cryptohub/',
     githublink: 'https://github.com/r-ahmed2022/cryptohub',
-    description: 'A react app displaying cryptocurrency data',
+    description: 'A cryptocurrency SPA, displaying 24-hour info about crptocurrencies, besides statistical view ',
     techstack: ['html/css', 'javscript', 'React'],
   },
   {
@@ -100,7 +100,7 @@ export const reactPortfolio = [
         'images/spacetraveller.png',
     url: 'https://r-ahmed2022.github.io/SpaceTravellerHub/',
     githublink: 'https://github.com/r-ahmed2022/SpaceTravellerHub',
-    description: 'A react app for booking space travel',
+    description: 'A react SPA, giving space travel info, besides booking space travels',
     techstack: ['html/css', 'javscript', 'React'],
 
   },
@@ -111,8 +111,8 @@ export const reactPortfolio = [
           'images/quiziccal.png',
     url: 'https://r-ahmed2022.github.io/quizzical-app/',
     githublink: 'https://github.com/r-ahmed2022/quizzical-app',
-    description: 'A quiz app developed in react',
-    techstack: ['html/css', 'javscript', 'React'],
+    description: 'A react based quiz app ',
+    techstack: ['html/css', 'React'],
 
   },
 ];
@@ -125,7 +125,7 @@ export const railsPortfolio = [
           'images/laptop-carrentals.png',
     url: 'https://supercarsrentals.onrender.com/',
     githublink: 'https://github.com/Itswali/rental-cars-front-end',
-    description: 'A car rental app using react and rails',
+    description: 'A car rental app developed using react and rails 7',
     techstack: ['html/css', 'React', 'Rails'],
 
   },
@@ -136,7 +136,7 @@ export const railsPortfolio = [
         'images/recipeapp.png',
     url: 'https://recipemanager.onrender.com',
     githublink: 'https://github.com/r-ahmed2022/Recipe-App',
-    description: 'A recipe manager app  developed in Rails',
+    description: 'A full-stack Recipe app developed in Rails 7',
     techstack: ['html/css', 'React', 'Rails'],
   },
   {
@@ -146,7 +146,7 @@ export const railsPortfolio = [
         'images/budgetapp.png',
     url: 'https://budget-app-gj5b.onrender.com/',
     githublink: 'https://github.com/r-ahmed2022/budgetapp',
-    description: 'Money management app',
+    description: 'A full-stack expense Tracker app using Rails',
     techstack: ['html/css', 'React', 'Rails'],
   },
   {
@@ -156,7 +156,7 @@ export const railsPortfolio = [
         'images/blogsphere.png',
     url: 'https://blogsphere-online.onrender.com/',
     githublink: 'https://github.com/r-ahmed2022/blogsphere',
-    description: 'A blog app developed using Rails ',
+    description: 'A full-stack blogs app, developed in rails, with devise authentication ',
     techstack: ['html/css', 'React', 'Rails'],
   },
 ];
